c9976774145961c1dac13f6731af9ca8000adac3
Ben <benjamincheeseman18@gmail.com>
2017-10-19 14:36:50 +0000

    added get requests

--- expressApp/app.js
+++ expressApp/app.js
@@ -1,9 +1,35 @@
 let express = require('express'); 
 let app = express(); 
 
+
+
+
 app.get("/", function(req, res) {
     res.send('Hi There!'); 
-}); 
+});
+
+app.get("/bye", function(req, res) {
+    res.send('Bye!'); 
+});
+
+app.get("/dog", function(req, res) {
+    res.send('Meow!'); 
+});
+
+app.get("/r/:subredditName", function(req, res) {
+    let subreddit = req.params.subredditName; 
+    res.send('Welcome to the ' + subreddit.toUpperCase() + ' page!!'); 
+});
+
+app.get("/r/:subredditName/comments/:id/:title", function(req, res) {
+    console.log(req.params); 
+    res.send('Welcome to the comments page'); 
+});
+
+
+app.get("*", function(req, res) {
+    res.send('YOU ARE A STAR'); 
+});
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log('The Server Has Started!!!')
